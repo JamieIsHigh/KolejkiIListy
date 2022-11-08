f8caecdc85d7a06be92d5ae0cf87c4f8c3acecd4
Filippo0420 <fpawelec0@gmail.com>
2022-11-08 11:19:22 +0100

    Initial commit

---
+++ app/src/main/java/com/fpp/fifolifo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fpp.fifolifo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
