6494ff4818f3597c03afb50186523175b100fdae
kstajbnz <k.stajkowski@o2.pl>
2022-11-08 16:40:14 +0100

    Lista istnieje :D

--- app/src/main/java/com/fpp/fifolifo/MainActivity.kt
+++ app/src/main/java/com/fpp/fifolifo/MainActivity.kt
@@ -2,10 +2,14 @@ package com.fpp.fifolifo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var mushrooms = Stack<String>()
+        mushrooms.addAll(listOf("borowik", "maślak", "kurka"))
     }
 }
