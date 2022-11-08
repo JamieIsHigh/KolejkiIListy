d97e00e1594b34b287db3a9e19043d75b0948eff
kstajbnz <k.stajkowski@o2.pl>
2022-11-08 16:55:12 +0100

    Dodawanie do listy

--- app/src/main/java/com/fpp/fifolifo/MainActivity.kt
+++ app/src/main/java/com/fpp/fifolifo/MainActivity.kt
@@ -2,6 +2,9 @@ package com.fpp.fifolifo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,10 @@ class MainActivity : AppCompatActivity() {
 
         var mushrooms = Stack<String>()
         mushrooms.addAll(listOf("borowik", "maślak", "kurka"))
+        //findViewById<TextView>(R.id.listView).text = mushrooms.peek()
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            mushrooms.push(findViewById<EditText>(R.id.textInput).text.toString())
+        }
     }
 }
