bd7324101295ccb42668f48569ef19a96cb6ad75
kstajbnz <k.stajkowski@o2.pl>
2022-11-08 17:32:32 +0100

    Ostatnia poprawka id

--- app/src/main/java/com/fpp/fifolifo/MainActivity.kt
+++ app/src/main/java/com/fpp/fifolifo/MainActivity.kt
@@ -16,11 +16,11 @@ class MainActivity : AppCompatActivity() {
         mushrooms.addAll(listOf("borowik", "maślak", "kurka"))
         findViewById<TextView>(R.id.listView).text = mushrooms.peek()
 
-        findViewById<Button>(R.id.button3).setOnClickListener {
+        findViewById<Button>(R.id.button4).setOnClickListener {
             mushrooms.push(findViewById<EditText>(R.id.textInput).text.toString())
             findViewById<TextView>(R.id.listView).text = mushrooms.peek()
         }
-        findViewById<Button>(R.id.button4).setOnClickListener {
+        findViewById<Button>(R.id.button2).setOnClickListener {
             if (!mushrooms.isEmpty()) {
                 mushrooms.pop()
                 if (mushrooms.size == 0) {
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         ksiazki.addAll(listOf("Harry Potter", "Hunger Games"))
         findViewById<TextView>(R.id.textView2).text = ksiazki.peek()
         findViewById<TextView>(R.id.textView3).text = "Długość kolejki: " + ksiazki.size
-        findViewById<Button>(R.id.button2).setOnClickListener {
+        findViewById<Button>(R.id.button3).setOnClickListener {
             if (!ksiazki.isEmpty()) {
                 ksiazki.removeFirst()
                 if (ksiazki.size == 0) {
